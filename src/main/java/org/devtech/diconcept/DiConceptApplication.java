21355e8cacc4a548131b57c1d99374d30641f301
devtechsolution <srivastvadevzone@gmail.com>
2019-01-15 19:20:05 +0530

    Initial commit

---
+++ src/main/java/org/devtech/diconcept/DiConceptApplication.java
@@ -0,0 +1,14 @@
+package org.devtech.diconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiConceptApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/devtech/diconcept/DiConceptApplicationTests.java
@@ -0,0 +1,17 @@
+package org.devtech.diconcept;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiConceptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
