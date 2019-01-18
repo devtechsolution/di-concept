c3861142774804f5f1ee9fb1ed1596da07226ece
devtechsolution <srivastvadevzone@gmail.com>
2019-01-18 22:09:15 +0530

    Added MyController

--- src/main/java/org/devtech/diconcept/DiConceptApplication.java
+++ src/main/java/org/devtech/diconcept/DiConceptApplication.java
@@ -1,14 +1,24 @@
 package org.devtech.diconcept;
 
+import org.devtech.diconcept.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiConceptApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiConceptApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DiConceptApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
+
 	}
+
 
 }
 
